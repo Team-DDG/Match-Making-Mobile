06a7eacd84806b81f5b605c1deff3031e9afa13f
kimdohun0104 <dikolight203@gmail.com>
2020-06-23 09:55:26 +0900

    [UI] Find password ui and dialog

--- lib/ui/screen/email_login_page.dart
+++ lib/ui/screen/email_login_page.dart
@@ -3,6 +3,7 @@ import 'package:match_making/ui/colors.dart';
 import 'package:match_making/ui/common/common_app_bar.dart';
 import 'package:match_making/ui/common/common_button.dart';
 import 'package:match_making/ui/common/common_text_field.dart';
+import 'package:match_making/ui/styles.dart';
 
 class EmailLoginPage extends StatelessWidget {
   @override
@@ -10,14 +11,14 @@ class EmailLoginPage extends StatelessWidget {
     return Scaffold(
       appBar: CommonAppBar(text: '로그인'),
       body: Container(
-        padding: EdgeInsets.only(left: 48, bottom: 48, right: 48, top: 36),
+        padding: padding48,
         child: Column(
           children: <Widget>[
             CommonTextField(hint: '이메일'),
             SizedBox(height: 10),
             CommonTextField(hint: '비밀번호'),
             SizedBox(height: 10),
-            _buildAccountRow(),
+            _buildAccountRow(context),
             Expanded(child: Container()),
             CommonButton(
               text: '로그인',
@@ -28,23 +29,31 @@ class EmailLoginPage extends StatelessWidget {
     );
   }
 
-  _buildAccountRow() => Row(
+  _buildAccountRow(BuildContext context) => Row(
         children: <Widget>[
-          Text(
-            '계정이 없으신가요?',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'RIXGOM',
-              fontSize: 10,
+          GestureDetector(
+            onTap: () {},
+            child: Text(
+              '계정이 없으신가요?',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'RIXGOM',
+                fontSize: 10,
+              ),
             ),
           ),
           Expanded(child: Container()),
-          Text(
-            '비밀번호를 잊으셨나요?',
-            style: TextStyle(
-              fontSize: 10,
-              color: colorLol,
-              fontFamily: 'RIXGOM',
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/findPassword');
+            },
+            child: Text(
+              '비밀번호를 잊으셨나요?',
+              style: TextStyle(
+                fontSize: 10,
+                color: colorLol,
+                fontFamily: 'RIXGOM',
+              ),
             ),
           )
         ],
