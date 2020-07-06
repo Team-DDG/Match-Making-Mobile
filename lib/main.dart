2d9d6248b1a3f49b567033d2d4d301ddbcad0758
seung min <3235tmdals@gmail.com>
2020-07-06 12:09:28 +0900

    [UI] Finished Leave Service Page

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:match_making/ui/colors.dart';
 import 'package:match_making/ui/page/change_password_page.dart';
 import 'package:match_making/ui/page/email_login_page.dart';
 import 'package:match_making/ui/page/find_password_page.dart';
+import 'package:match_making/ui/page/leave_service_page.dart';
 import 'package:match_making/ui/page/login_method_page.dart';
 import 'package:match_making/ui/page/main_page.dart';
 import 'package:match_making/ui/page/report_page.dart';
@@ -26,7 +27,8 @@ class MatchMakingApp extends StatelessWidget {
         '/selfCertification': (_) => SelfCertificationPage(),
         '/main': (_) => MainPage(),
         '/changePassword': (_) => ChangePasswordPage(),
-        '/reportPage': (_) => ReportPage()
+        '/reportPage': (_) => ReportPage(),
+        '/leaveService': (_) => LeaveServicePage()
       },
       theme: ThemeData(
         canvasColor: Colors.black,
