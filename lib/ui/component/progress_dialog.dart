250e98aa2c123fc0bf32b65f30525b9817253165
kimdohun0104 <dikolight203@gmail.com>
2020-07-14 13:57:27 +0900

    [UPDATE] Use cascade notation을 사용하여 해결

--- lib/ui/component/progress_dialog.dart
+++ lib/ui/component/progress_dialog.dart
@@ -2,12 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:progress_dialog/progress_dialog.dart';
 
 ProgressDialog getProgressDialog(BuildContext context, String message) {
-  var progressDialog = ProgressDialog(
+  return ProgressDialog(
     context,
     type: ProgressDialogType.Normal,
     isDismissible: false,
     showLogs: false,
-  );
-  progressDialog.style(message: message);
-  return progressDialog;
+  )..style(message: message);
 }
