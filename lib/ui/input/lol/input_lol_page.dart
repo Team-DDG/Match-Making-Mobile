8e3309f24d0707134b90310ac7fc805fd95c241c
seung min <3235tmdals@gmail.com>
2020-08-11 15:07:48 +0900

    [Add] added model to input lol page to request api

--- lib/ui/input/lol/input_lol_page.dart
+++ lib/ui/input/lol/input_lol_page.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:match_making/ui/colors.dart';
 import 'package:match_making/ui/component/common_app_bar.dart';
+import 'package:match_making/ui/input/input_profile_model.dart';
 import 'package:match_making/ui/input/lol/component/input_lol_body.dart';
+import 'package:provider/provider.dart';
 
 class InputLolPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<InputProfileModel>();
     return Scaffold(appBar: CommonAppBar(
       text: '정보 등록',
       actionWidgets: <Widget>[
         Center(
           child: GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(context, '/input/lol');
-            },
-            child: Text(
-              '다음',
-              style: TextStyle(
-                color: colorLol,
-                fontSize: 18,
-              ),
-            ),
+              onTap: () {
+                Navigator.pushNamed(context, '/input/lol');
+              },
+              child: GestureDetector(
+                onTap: () {
+
+                },
+                  child: Text(
+                    '다음',
+                    style: TextStyle(
+                      color: colorLol,
+                      fontSize: 18,
+                    ),
+                  ), ,
+              )
           ),
         ),
         SizedBox(width: 24)
