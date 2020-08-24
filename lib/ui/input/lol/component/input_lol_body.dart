7f759a998ac72d8c2c31d0be910082b7bd259a18
kimdohun0104 <kimdohun0104@gmail.com>
2020-08-24 21:24:21 +0900

    [UPDATE] context.showAndGetProgressDialog() 구현

--- lib/ui/input/lol/component/input_lol_body.dart
+++ lib/ui/input/lol/component/input_lol_body.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:match_making/data/error/handling_method_type.dart';
 import 'package:match_making/data/response/lol_response.dart';
 import 'package:match_making/extension/context_ext.dart';
-import 'package:match_making/ui/component/progress_dialog.dart';
 import 'package:match_making/ui/input/input_profile_model.dart';
 import 'package:match_making/ui/input/lol/component/rank_info_widget.dart';
 import 'package:match_making/ui/input/lol/component/user_info_widget.dart';
@@ -35,23 +34,7 @@ class _InputLolBodyState extends State<InputLolBody> {
                 decoration: InputDecoration(
                     hintText: '소환사명을 입력하세요',
                     suffixIcon: IconButton(
-                      onPressed: () async {
-                        final progressBar =
-                            getProgressDialog(context, '조회중입니다...');
-                        await progressBar.show();
-                        model
-                            .getLolBySummonerName({
-                              'summonerName': _lolInputController.value.text
-                            })
-                            .catchError((e) => {
-                                  if (e is Navigate)
-                                    Navigator.pushReplacementNamed(
-                                        context, e.route)
-                                  else if (e is Message)
-                                    context.showSnackbar(e.message)
-                                })
-                            .whenComplete(() => progressBar.hide());
-                      },
+                      onPressed: _onClickSearchSummoner(context, model),
                       icon: Icon(Icons.send),
                     )),
               ),
@@ -86,5 +69,18 @@ class _InputLolBodyState extends State<InputLolBody> {
     } else {
       return Container();
     }
+  }
+
+  _onClickSearchSummoner(BuildContext context, InputProfileModel model) async {
+    final progressBar = await context.showAndGetProgressDialog('조회중입니다...');
+    model
+        .getLolBySummonerName({'summonerName': _lolInputController.value.text})
+        .catchError((e) => {
+              if (e is Navigate)
+                Navigator.pushReplacementNamed(context, e.route)
+              else if (e is Message)
+                context.showSnackbar(e.message)
+            })
+        .whenComplete(() => progressBar.hide());
   }
 }
