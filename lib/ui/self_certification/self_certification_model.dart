d467878c09df795ad4019948479f92730f206ec4
seung min <3235tmdals@gmail.com>
2020-08-26 18:56:54 +0900

    [Add] 임시 개발한 전화번호 인증 서버용 model 추가

--- lib/ui/self_certification/self_certification_model.dart
+++ lib/ui/self_certification/self_certification_model.dart
@@ -1,13 +1,18 @@
+import 'package:match_making/data/error/exception/unauthorized_exception.dart';
 import 'package:match_making/data/error/handling_method_type.dart';
+import 'package:match_making/data/service/phone_auth_service.dart';
 import 'package:match_making/provider/base_model.dart';
 
 class SelfCertificationModel extends BaseModel {
+  final PhoneAuthService _phoneAuthService;
 
-  Future verifyPhone(String phone) async {
+  SelfCertificationModel(this._phoneAuthService);
+
+  Future verifyPhone(String phone, String code) async {
     try {
-
-    } catch(e) {
-      return Future.error(Message(e.toString()));
+      _phoneAuthService.verifyPhone(phone, code);
+    } on UnauthorizedException {
+      return Future.error(Message('인증 번호가 올바르지 않습니다'));
     }
   }
 }
