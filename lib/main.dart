66ea2e6791159f549f62f5ad841fedc4c4429f84
seung min <3235tmdals@gmail.com>
2020-09-22 09:52:30 +0900

    [Error] Ad failed with code 0 Log: <Google:HTML> Not all asset views lie inside the native ad view. This indicates an integration problem. Such implementations will not be supported in the future. Please make sure that all native ad assets are rendered inside the native ad view.

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MatchMakingApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/input/lol',
+        initialRoute: '/main',
         routes: {
           '/login/method': (_) => LoginMethodPage(),
           '/login/email': (_) => EmailLoginPage(),
--- lib/ui/page/matching_page.dart
+++ lib/ui/page/matching_page.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_admob/flutter_native_admob.dart';
 import 'package:match_making/ui/component/common_app_bar.dart';
 import 'package:match_making/ui/component/common_button.dart';
 
 class MatchingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final APP_ID = 'ca-app-pub-5565379258849542~8235036520';
+    final add_id = 'ca-app-pub-5565379258849542/1669628174';
+    //TODO failed with code 0. it seems i newly added new ads
+    final test_id = 'ca-app-pub-3940256099942544/2247696110';
     return Scaffold(
       appBar: CommonAppBar(text: '매칭'),
       body: Center(
         child: Column(
           children: <Widget>[
+            Container(
+              height: 60,
+              child: NativeAdmob(
+                adUnitID: test_id,
+              ),
+            ),
+            Expanded(
+              child: Container(),
+            ),
             CircularProgressIndicator(),
             SizedBox(
               height: 30,
@@ -22,12 +36,12 @@ class MatchingPage extends StatelessWidget {
               '???초 경과',
               style: TextStyle(color: Colors.white, fontSize: 16),
             ),
-            SizedBox(
-              height: 30,
+            Expanded(
+              child: Container(),
             ),
             CommonButton(
               text: 'STOP!',
-              onPressed: () {  },
+              onPressed: () {},
             )
           ],
           crossAxisAlignment: CrossAxisAlignment.center,
